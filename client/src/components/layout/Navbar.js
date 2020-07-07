6824e9c95d7d0409f193efd0dec76741552fa653
harry walker <harrywalker312@gmail.com>
2020-07-07 16:25:16 +0100

    navabr authentication and documentation

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/AuthContext";
 
 export const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+
+  const authLinks = (
+    <>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a href="!#">
+          <i className="fas fa-sign-out-alt"></i>
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </>
+  );
+  const guestLinks = (
+    <>
+      <li>
+        <Link to="/Register">Register</Link>
+      </li>
+      <li>
+        <Link to="/Login">Login</Link>
+      </li>
+    </>
+  );
+
   return (
-    <div className="navbar">
+    <div className="navbar bg-primary">
       <h1>
-        <i className={icon} /> {title}
+        <Link to="/">
+          <i className={icon} /> {title}
+        </Link>
       </h1>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
+        {isAuthenticated ? authLinks : guestLinks}
         <li>
           <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/Register">Register</Link>
-        </li>
-        <li>
-          <Link to="/Login">Login</Link>
         </li>
       </ul>
     </div>
   );
 };
-
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string,
