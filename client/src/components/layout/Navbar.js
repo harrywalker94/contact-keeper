67a171edafad79571b15c4c8bed96966d6ab4a5e
harry walker <harrywalker312@gmail.com>
2020-07-19 11:33:32 +0100

    final

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,37 +1,47 @@
-import React, { useContext } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import AuthContext from "../../context/auth/AuthContext";
+import ContactContext from "../../context/contact/contactContext";
 
-export const Navbar = ({ title, icon }) => {
+const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
 
-  const { isAuthenticated, logout, user } = authContext;
+  const { isAuthenticated, logout, user, loadUser } = authContext;
+  const { clearContacts } = contactContext;
+
+  useEffect(() => {
+    loadUser();
+    // eslint-disable-next-line
+  }, []);
 
   const onLogout = () => {
     logout();
-  }
+    clearContacts();
+  };
 
   const authLinks = (
-    <>
+    <Fragment>
       <li>Hello {user && user.name}</li>
       <li>
-        <a href="!#" onClick={onLogout}>
-          <i className="fas fa-sign-out-alt"></i>
+        <a onClick={onLogout} href="#!">
+          <i className="fas fa-sign-out-alt" />{" "}
           <span className="hide-sm">Logout</span>
         </a>
       </li>
-    </>
+    </Fragment>
   );
+
   const guestLinks = (
-    <>
+    <Fragment>
       <li>
-        <Link to="/Register">Register</Link>
+        <Link to="/register">Register</Link>
       </li>
       <li>
-        <Link to="/Login">Login</Link>
+        <Link to="/login">Login</Link>
       </li>
-    </>
+    </Fragment>
   );
 
   return (
@@ -50,14 +60,15 @@ export const Navbar = ({ title, icon }) => {
     </div>
   );
 };
+
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string,
 };
 
 Navbar.defaultProps = {
-  title: "Contact App",
-  icon: "fas fa-address-card",
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt",
 };
 
 export default Navbar;
